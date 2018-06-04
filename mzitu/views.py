66667259a171ff25172fe35ba0b465445d7e2418
26huitailang <26huitailang@gmail.com>
2018-06-05 00:19:54 +0800

    [Update]相关方法改为class
    todo：theme存储，放入一个文件夹，celery异步任务

--- mzitu/views.py
+++ mzitu/views.py
@@ -7,8 +7,8 @@ from mzitu.constants import (
     PROXY_SOURCE_URL,
     USER_AGENT_LIST,
 )
-from mzitu.runtimes.url_parser import download_one_suit
-from mzitu.runtimes.asyncio_for_mzitu import async_get_images_urls
+from mzitu.runtimes.url_parser import MzituOneSuite, MzituThemePage
+# from mzitu.runtimes.asyncio_for_mzitu import async_get_images_urls
 
 
 def index(request):
@@ -18,10 +18,23 @@ def index(request):
 
 def parse_and_download_one_suit(request):
 
-    suit_url = request.GET['suit_url']
+    suite_url = request.GET['suite_url']
 
     # 线程
-    download_one_suit(suit_url)
+    mzitu_one_suite = MzituOneSuite(suite_url)
+    mzitu_one_suite.download_one_suit()
+
+    return HttpResponse("下载完成")
+
+
+def download_one_theme(request):
+    theme_url = request.GET['theme_url']
+
+    mzitu_theme = MzituThemePage(theme_url)
+    suite_url_list = mzitu_theme.get_suite_urls_to_redis()
+    for i in suite_url_list:
+        mzitu_one_suite = MzituOneSuite(i)
+        mzitu_one_suite.download_one_suit()
 
     return HttpResponse("下载完成")
 
@@ -36,9 +49,9 @@ def get_proxies(request):
 
 def async_parse_and_download_one_suit(request):
 
-    suit_url = request.GET['suit_url']
+    suite_url = request.GET['suite_url']
 
     # 线程
-    async_get_images_urls(suit_url)
+    # async_get_images_urls(suite_url)
 
     return HttpResponse("下载完成")
