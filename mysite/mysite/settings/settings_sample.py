72aa26b8dd4b0cb938f8dc7b438fe2fcb4c5855c
26huitailang <26huitailang@gmail.com>
2018-07-12 17:28:46 +0800

    channels: synchronous ChatConsumer

--- mysite/mysite/settings/settings_sample.py
+++ mysite/mysite/settings/settings_sample.py
@@ -1,5 +1,17 @@
+# coding: utf-8
 from .base import *
 
 DEBUG = True
 
-DATABASES['default']['NAME'] = 'sample.sqlite3'
+DATABASES['default']['NAME'] = 'local.sqlite3'
+
+# Channels
+ASGI_APPLICATION = 'mysite.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)]
+        }
+    }
+}
