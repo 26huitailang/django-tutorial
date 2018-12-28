d6d8110ccfa5ec8b0cf67de27aac3610477e0118
26huitailang <26huitailang@gmail.com>
2018-12-28 15:09:36 +0800

    feat(vue mzitu): Suites界面适应1000px边界的两种布局

--- mysite/frontend/src/components/MzituSuiteCard.vue
+++ mysite/frontend/src/components/MzituSuiteCard.vue
@@ -2,18 +2,21 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <el-row>
-      <el-col :span="8" v-for="(item, index) in suites" :key="item.id" :offset="index > 0 ? 2 : 0">
-        <el-card :body-style="{ padding: '0px', height: '180px' }" shadow="hover">
+      <el-col :span="elColSpan" v-for="(item, index) in suites" :key="item.id" :offset="elColOffset">
+        <el-card :body-style="{ padding: '10px' }" shadow="hover">
           <!-- todo: development usage -->
-          <img :src="'http://127.0.0.1:8000' + item.images[0].image" class="image">
+          <div class="cover">
+            <img :src="'http://192.168.2.101:8000' + item.images[0].image" class="image">
+          </div>
           <div style="padding: 14px;">
             <router-link
               :to="{ name: 'mzitu-suite-detail', params: { id: item.id }}"
             >{{ index }}. {{ item.name }} {{item.max_page }}</router-link>
             <div class="bottom clearfix">
               <!-- todo: loop for tags -->
+              <el-tag size="mini">标签1</el-tag>
               <el-tag size="mini">标签3</el-tag>
-              <el-button type="text" class="button">标签2</el-button>
+              <!-- <el-button type="text" class="button">标签2</el-button> -->
             </div>
           </div>
         </el-card>
@@ -28,6 +31,26 @@ export default {
   props: {
     msg: String,
     suites: Array
+  },
+  data () {
+    return {
+      elColSpan: 22,
+      elColOffset: 0,
+    }
+  },
+  beforeMount: function () {
+    //可用于设置自适应屏幕，根据获得的可视宽度（兼容性）判断是否显示
+    let w = document.documentElement.offsetWidth || document.body.offsetWidth;
+    if (w < 1000) {
+      this.elColSpan = 22
+    } else {
+      this.elColSpan = 8
+    }
+    if (this.elColSpan > 8) {
+      this.elColOffset = 1
+    } else {
+      this.elColOffset = 0
+    }
   }
 };
 </script>
@@ -49,9 +72,23 @@ export default {
   float: right;
 }
 
+/* .cover {
+  margin: 10px auto;
+  width: 100%;
+  height: 240px;
+  overflow: hidden;
+  position: relative;
+} */
 .image {
   width: 100%;
+  height: 300px;
+  left: 50%;
   display: block;
+  object-fit: cover;
+  position: relative;
+  -webkit-transform: translateX(-50%);
+	-ms-transform: translateX(-50%);
+	-moz-transform: translateX(-50%);
 }
 
 .clearfix:before,
