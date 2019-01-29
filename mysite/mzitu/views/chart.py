e36ea2517e111f817ce775cdfee200166522f621
26huitailang <26huitailang@gmail.com>
2019-01-29 18:10:03 +0800

    feat(vue echarts): Tag的横向柱状图，ProxyIp的散点图
    - 有颜色区分，使用visualMap去配置
    - echarts多看文档和官方示例就行

--- mysite/mzitu/views/chart.py
+++ mysite/mzitu/views/chart.py
@@ -11,48 +11,35 @@ from mzitu.serializers import MzituDownloadedSuiteSerializer, TagSerializer
 
 from django_vises.runtimes.misc import sort_dict_list
 
+
 class ChartViewSet(GenericViewSet):
     serializer_class = MzituDownloadedSuiteSerializer
     queryset = ProxyIp.objects
 
     @action(methods=['GET'], detail=False)
-    def test(self, request):
-        """获取新的proxy ip"""
+    def tags_bar(self, request):
         tag_objs = Tag.objects.all()
         serializer_data = TagSerializer(tag_objs, many=True).data
-        serializer_data_order_by_suites_count = sort_dict_list(serializer_data, 'suites_count', reverse=True)
+        serializer_data_order_by_suites_count = sort_dict_list(serializer_data, 'suites_count')
         title = 'Tags统计'
-        # x_axis =
+        data = [x['suites_count'] for x in serializer_data_order_by_suites_count]
         response = {
-            'title': {
-                'text': title
-            },
-            'legend': {
-                'data': ['bar']
-            },
-            'tooltip': {
-            },
-            'xAxis': {
-                'name': 'TagName',
-                'data': [x['name'] for x in serializer_data_order_by_suites_count],
-                'nameRotate': 30,
-            },
-            'yAxis': {},
-            'series': [
-                {
-                    # // cozordinateSystem: 'polar',
-                    'name': 'line',
-                    'type': 'bar',
-                    'showSymbol': False,
-                    'data': [x['suites_count'] for x in serializer_data_order_by_suites_count]
-                }
-            ],
-            'animationDuration': 2000
+            'title': title,
+            'axisLabel': [x['name'] for x in serializer_data_order_by_suites_count],
+            'data': data,
+            'maxCount': max(data) if data else 0
         }
 
         return Response(response, status=status.HTTP_200_OK)
 
-    # @action(detail=False, methods=['post'])
-    # def check_valid(self, request):
-    #     """标记失效的代理"""
-    #     return
+    @action(methods=['GET'], detail=False)
+    def proxyips_bar(self, request):
+        query_data = ProxyIp.group_by_score()
+        title = 'ProxyIp score统计'
+        count_list = [x[1] for x in query_data]
+        response = {
+            'title': title,
+            'data': query_data,
+            'maxCount': max(count_list) if count_list else 0
+        }
+        return Response(response, status=status.HTTP_200_OK)
