80b6726aed4719baa6d2b3c449214768fe14713a
26huitailang <26huitailang@gmail.com>
2019-03-25 23:48:47 +0800

    fix(regexp): add `&`

--- mysite/django_vises/runtimes/files.py
+++ mysite/django_vises/runtimes/files.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 def rename_file_or_folder_name(name: str) -> str:
     """替换路径或文件中的非法字符"""
-    new_name = re.sub(r'[/\\:*?"<>|]', '-', name)  # windows 非法文件夹名字符
+    new_name = re.sub(r'[/\\:*?"<>|&]', '-', name)  # 非法文件夹名字符
     if new_name != name:
         logger.info("name: {} | new: {}".format(name, new_name))
     return new_name
